321f6d872b2aaf84647c21a78c8cd6d547238f17
hezronkimutai <hezronchelimo.hc@gmail.com>
2020-02-15 20:13:04 +0200

    increase test coverage

--- src/pagination.js
+++ src/pagination.js
@@ -18,20 +18,22 @@ export default ({
 }) => {
   const [pageNo, setPageNo] = useState(0)
 
-  const pages = data ? Paginate(data, itemsPerPage) : []
+  const pages = data.length && Paginate(data, itemsPerPage)
 
-  const allPages = pages.map((item, index) => index)
+  const allPages = data.length && pages.map((item, index) => index)
 
   const allPageNumbers =
-    pageNo < Math.round(pageButtons / 2)
+    data.length &&
+    (pageNo < Math.round(pageButtons / 2)
       ? allPages.slice(0, pageButtons)
       : allPages.slice(
           pageNo - Math.round(pageButtons / 2),
           pageNo + Math.round(pageButtons / 2)
-        )
+        ))
   return (
     <div className="pagination-container">
-      {(pages[pageNo] || pages[0]).map((item, index) => onePage(item, index))}
+      {data.length &&
+        (pages[pageNo] || pages[0]).map((item, index) => onePage(item, index))}
       <div className="pagination-buttons">
         <div className="arrows">
           <button
@@ -43,18 +45,19 @@ export default ({
           </button>
         </div>
         <div className="pages">
-          {allPageNumbers.map((item, index) => (
-            <div id={index} key={index}>
-              <button
-              id='kill'
-                type="button"
-                onClick={() => setPageNo(item)}
-                style={item === pageNo ? activePageStyle : {}}
-              >
-                {pageName} {item + 1}
-              </button>
-            </div>
-          ))}
+          {data.length &&
+            allPageNumbers.map((item, index) => (
+              <div id={index} key={index}>
+                <button
+                  id="kill"
+                  type="button"
+                  onClick={() => setPageNo(item)}
+                  style={item === pageNo ? activePageStyle : {}}
+                >
+                  {pageName} {item + 1}
+                </button>
+              </div>
+            ))}
         </div>
         <div className="arrows">
           <button
